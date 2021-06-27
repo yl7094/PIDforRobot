f3b6682991203d285355e3ad417b05998a8849b3
yl7094 <grys2216787@gmail.com>
2021-06-26 21:23:28 -0400

    add humanoid

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import numpy as np
-import matplotlib.pyplot as plt
+
 from robot import Robot
 from pid import PID
-T = 5000 # needs to define
+
+T = 2000
 dt = 1e-3
-p_gain = 10. # needs to define
-d_gain = 10./np.sqrt(2) # needs to define
-x0 = np.zeros([2, 1])  # needs to define
+rest_T = 500
+time1 = np.linspace(0, T*dt, T-rest_T)
+time2 = np.linspace(T*dt, T*dt, rest_T) # for tracking restitution
+time = np.concatenate([time1, time2])
+p_gain = 25.        # for balance with noise, p_gain=20
+d_gain = 0.2        # for balance with noise, d_gain=0.1
+x0 =np.array([[0.],[0.]]) #rad, rad/s
+
 u_list = [0.]*T
 x_list = [None] *T
-theta_des = 0.
-u_des = 0. 
+
+# theta_des = [0.]*T # for balance
+
+amplitude = np.pi/10. # for tracking
+theta_des = np.sin(time/(T*dt)*2*np.pi)*amplitude
+
+u_des = 0.
 t = 0.
+
+
 robot = Robot(x0)
 pid = PID(p_gain,0.,d_gain, t)
+
 for i in range(T):
-	print('simulation step: %d'%i)
-	x = robot.getState()
-	#######controller#######
-	theta = x[0,0]
-	pid.SetPoint = theta_des
-	pid.update(theta, t)
-	o=pid.output
-	u = u_des - o
-	#######controller#######
-	x_list[i] = x
-	u_list[i] = u
-	print(x_list)
-	print(u_list)
-	robot.step(u)
-	t += dt
+
+    print('simulation step: %d'%(i+1))
+    
+    x = robot.getState()
+    
+    #######controller#######
+    theta = x[0,0]
+    pid.SetPoint = theta_des[i]
+    pid.update(theta, t)
+    o=pid.output
+    u = robot.bound(o - u_des)
+    #######controller#######
+
+    x_list[i] = x
+    u_list[i] = u
+    robot.step(u, i)
+    t += dt
+
+robot.plot(x_list, theta_des, u_list)
+
+robot.animate(x_list, theta_des, u_list, speed=.3)
--- robot.py
+++ robot.py
@@ -1,27 +1,129 @@
+from matplotlib.pyplot import plot
 import numpy as np
+import matplotlib as mp
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 class Robot:
 	def __init__(self, x0):
 		'''
 		this is a linear inverted pendulum model with ankle strategy
 		'''
 		self.x = x0
-		self.A = np.zeros([2, 2]) # needs to define
-		self.B = np.zeros([2, 1]) # needs to define
-		K,m,g,L,C=753,5,9.81,0.3,18
+		self.A = np.zeros([2, 2]) 
+		self.B = np.zeros([2, 1]) 
+		K,m,g,L,C = 50.,5.,9.81,0.3,10.
 		dt= 1e-3
 		I= np.eye(2)
-		print(I)
-		matrix_a= np.matrix([[0,1], [-(K*m*g*L/(m*L**2)),-(C/(m*L**2))]])
+		matrix_a= np.matrix([[0,1], [-(K-m*g*L)/(m*L**2),-(C/(m*L**2))]])
 		matrix_b= np.matrix([[0],[(K/(m*L**2))]])
-		A= dt * matrix_a + I
-		B= dt * matrix_b
-		#Example
-		#A= np.matrix([[1, 1*10**-3], [-(24.623), (0.96)]])
-		#B= np.matrix([[0],[(1.673)]])
-			   #Assuming the random values as: m=5kg, L=0.3m, g=9.81m/s**2, K=753Nm/rad, C=18Nm/sec, dt=1e-3, I=Identity Matrix of order(2x2)
-			   #Substituting these values in the general equation: Xn+1= AXn + BUn  such that A= dt * matrix_a + I  and  B= dt * matrix_b
-			   #Wherein, matrix_a= np.matrix([[0,1], [-(KmgL/mL**2),-(C/mL**2)]]) and matrix_b= np.matrix([[0],[(K/mL**2)]])
-	def step(self, u):
+		self.A= dt * matrix_a + I
+		self.B= dt * matrix_b
+
+		self.u_bounds = [-np.pi/4, np.pi/4]
+
+		self.noise = True
+		self.impulse = True
+               
+	def step(self, u, counter):
 		self.x= self.A.dot(self.x) + self.B.dot(u)
+		if counter >0 and self.noise:
+			disturbance = np.random.normal(0., 0.01)
+			self.x[1] +=disturbance
+		if counter ==500 and self.impulse:
+			impulse = 10.
+			self.x[1] += impulse
 	def getState(self):
 		return self.x.copy()
+
+	def bound(self, u):
+		if u<self.u_bounds[0]:
+			u_ret = self.u_bounds[0]
+			return u_ret
+		if u>self.u_bounds[1]:
+			u_ret = self.u_bounds[0]
+			return u_ret
+		return u
+	def plot(self, x_list, theta_des, u_list):
+		print('generate plot ...')
+		plt.figure()
+		plt.plot([x[0] for x in x_list])
+		plt.plot(theta_des, 'r--')
+		plt.ylabel(r'$\theta_x$')
+		plt.grid()
+		plt.savefig('data/theta.png',bbox_inches='tight',pad_inches = 0, dpi = 300)
+
+		plt.figure()
+		plt.plot([x[1] for x in x_list])
+		plt.ylabel(r'$\dot{\theta_x}$')
+		plt.grid()
+		plt.savefig('data/theta_dot.png',bbox_inches='tight',pad_inches = 0, dpi = 300)
+
+		plt.figure()
+		plt.plot(u_list)
+		plt.ylabel(r'$\theta_u$')
+		plt.grid()
+		plt.savefig('data/u.png',bbox_inches='tight',pad_inches = 0, dpi = 300)
+		print('generate plot ...done')
+	def animate(self, x_list, theta_des, u_list, speed=.3):
+		'''
+		This function makes an animation showing
+		the behavior of the single inverted pendulum model
+		'''
+
+		plotx = []
+		plotx_des = []
+		plotu = []
+		T = len(x_list)
+		freq = 30
+		sample_freq = int(freq*speed)
+		for i in range(int(T/sample_freq)):
+			plotx.append(x_list[sample_freq*i])
+			plotx_des.append(theta_des[sample_freq*i])
+			plotu.append(u_list[sample_freq*i])
+
+		use_dt = int(1000/freq)
+
+		fig = mp.figure.Figure(figsize=[2.4,2.4])
+		mp.backends.backend_agg.FigureCanvasAgg(fig)
+		ax = fig.add_subplot(111, autoscale_on=False, xlim=[-.4,.4], ylim=[-.1,.4])
+		ax.grid()
+		list_of_lines = []
+
+		#plot the ground
+		ax.plot([-0.3,0.3],  [0,0])
+
+		#create the robot
+		#for the desired CoM
+		line, = ax.plot([], [], 'or--', ms=10, lw=0, markevery=[-1], alpha=.3)
+		list_of_lines.append(line)
+		#for the simulated CoM
+		line, = ax.plot([], [], 'ok--', ms=10, lw=0.5, markevery=[-1])
+		list_of_lines.append(line)
+		#for the ankle
+		line, = ax.plot([], [], 'k', lw=1)
+		list_of_lines.append(line)
+
+		L = 0.3
+		lowerLeg =0.1
+		def animate(i):
+			for l in list_of_lines: #reset all lines
+				l.set_data([],[])
+			list_of_lines[0].set_data([0, L*np.sin(plotx_des[i])], [0, L*np.cos(plotx_des[i])])
+			list_of_lines[1].set_data([0, L*np.sin(plotx[i][0])], [0, L*np.cos(plotx[i][0])])
+			list_of_lines[2].set_data([0, lowerLeg*np.sin(plotu[i])], [0, lowerLeg*np.cos(plotu[i])])
+			
+			return list_of_lines
+		
+		def init():
+			return animate(0)
+		print('generate animation ...')
+		ani = animation.FuncAnimation(fig, animate, np.arange(0, len(plotx)),
+			interval=use_dt, blit=True, init_func=init)
+
+		ani.save('data/inverted_pendumlum.mp4',
+			dpi=300,
+			fps=freq,
+			writer='ffmpeg')
+
+		print('generate animation ...done')
